f530db2068f76d53fa54334d071d52f6cf484a57
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-31 23:42:10 -0500

    tests for POST /bookmarks/ implimented

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -8,25 +8,25 @@ function sanatizeAllBookmarks(inputedBookmarks) {
     let bookmarksToReturn;
     bookmarksToReturn = [];
     for (let i = 0; i < inputedBookmarks.length; i++) {
-      let thisBookmark = inputedBookmarks[i]
-      let thisBookmarkSanitized = thisBookmark
-      thisBookmarkSanitized.title = xss(thisBookmarkSanitized.title)
-      thisBookmarkSanitized.description = xss(thisBookmarkSanitized.description)
-      bookmarksToReturn.push(thisBookmarkSanitized)
+        let thisBookmark = inputedBookmarks[i]
+        let thisBookmarkSanitized = thisBookmark
+        thisBookmarkSanitized.title = xss(thisBookmarkSanitized.title)
+        thisBookmarkSanitized.description = xss(thisBookmarkSanitized.description)
+        bookmarksToReturn.push(thisBookmarkSanitized)
     }
     return bookmarksToReturn;
-  }
-  
-  function sanatizeOneBookmarks(inputedBookmark) {
+}
+
+function sanatizeOneBookmarks(inputedBookmark) {
     let bookmarkToReturn = {
-      id: inputedBookmark.id,
-      title: xss(inputedBookmark.title),
-      url: xss(inputedBookmark.url),
-      rating: inputedBookmark.rating,
-      description: xss(inputedBookmark.description)
+        id: inputedBookmark.id,
+        title: xss(inputedBookmark.title),
+        url: xss(inputedBookmark.url),
+        rating: inputedBookmark.rating,
+        description: xss(inputedBookmark.description)
     };
     return bookmarkToReturn;
-  }
+}
 
 let db
 
@@ -44,12 +44,237 @@ afterEach('cleanup', () => db('bookmarks').truncate())
 
 after('disconnect from db', () => db.destroy())
 
-//describe(`POST /bookmarks`, () => {
-//    context('Given all requirements are met for the new bookmark', () => {
-//        it('creates a bookmark, responding with 201 and the new bookmark', {
-//        })
-//    })
-//})
+describe.only(`POST /bookmarks`, () => {
+    context('Given all requirements are met for the new bookmark and url starts with http://', () => {
+        it('creates a bookmark, responding with 201 and the new bookmark', () => {
+            const newBookmark = {
+                title: 'New Bookmark Title',
+                description: 'New Bookmark Description',
+                rating: 5,
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    const expected = {
+                        id: 1,
+                        title: 'New Bookmark Title',
+                        description: 'New Bookmark Description',
+                        rating: 5,
+                        url: "http://newbookmarkurl.com"
+                    }
+                    const actual = res.body
+                    expect(actual).to.eql(expected)
+                })
+        })
+    })
+
+    context('Given all requirements are met for the new bookmark and url starts with https://', () => {
+        it('creates a bookmark, responding with 201 and the new bookmark', () => {
+            const newBookmark = {
+                title: 'New Bookmark Title',
+                description: 'New Bookmark Description',
+                rating: 5,
+                url: "https://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    const expected = {
+                        id: 1,
+                        title: 'New Bookmark Title',
+                        description: 'New Bookmark Description',
+                        rating: 5,
+                        url: "https://newbookmarkurl.com"
+                    }
+                    const actual = res.body
+                    expect(actual).to.eql(expected)
+                })
+        })
+    })
+
+    context('Given the title is missing from the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                description: 'New Bookmark Description',
+                rating: 5,
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given the URL is missing from the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                description: 'New Bookmark Description',
+                rating: 5,
+                title: 'New Bookmark Title',
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given the rating is missing from the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                description: 'New Bookmark Description',
+                url: "http://newbookmarkurl.com",
+                title: 'New Bookmark Title',
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given rating is not a number in the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: 'New Bookmark Title',
+                description: 'New Bookmark Description',
+                rating: "5",
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given rating is a number less than 1 in the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: 'New Bookmark Title',
+                description: 'New Bookmark Description',
+                rating: 0.9,
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given rating is a number greater than or equal to 6 in the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: 'New Bookmark Title',
+                description: 'New Bookmark Description',
+                rating: 6,
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given title is not a string in the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: true,
+                description: 'New Bookmark Description',
+                rating: 6,
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given description is not a string in the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: "New bookmark title",
+                description: 7,
+                rating: 6,
+                url: "http://newbookmarkurl.com"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given url is not a string in the new bookmark', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: "New bookmark title",
+                description: "New bookmark description",
+                rating: 6,
+                url: true
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+    context('Given url does not start with http:// or https://', () => {
+        it('Returns a 400 error', () => {
+            const newBookmark = {
+                title: "New bookmark title",
+                description: "New bookmark description",
+                rating: 6,
+                url: "invalidurl"
+            }
+
+            return supertest(app)
+                .post('/bookmarks/')
+                .send(newBookmark)
+                .expect(res => {
+                    expect(400)
+                })
+        })
+    })
+
+})
 
 describe(`Delete /bookmarks/:id`, () => {
     context('Given the bookmark ID exists', () => {
